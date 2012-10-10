cfa51f16603a6358611f1ca86559a48d1ebebd71
Aidan Kane <aidankane@gmail.com>
2012-10-10 08:44:59 +0000

    too many files

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
 class Entry(models.Model):
+    STATUS = (
+        ('pending', 'Pending'),
+        ('live', 'Live'),
+        ('archived', 'Archived'),
+    )
+
     title = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+    body = models.TextField()
+    date_published = models.DateTimeField()
+    status = models.CharField(max_length=20, choices=STATUS)
 
